d96bde32513efa61bbcb1debdde3c069c30ee2f5
unknown <2120367@talnet.nl>
2020-01-23 13:50:40 +0100

    Uitwerking van de exercise

---
+++ queries.sql
@@ -0,0 +1,5 @@
+use netland;
+
+--select title from series where rating > 2.5 ORDER by rating desc;
+--select title from series where seasons < 5 order by seasons asc;
+--select title from series where seasons < 3 || seasons > 20 order by seasons AND country asc;
